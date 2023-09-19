9320c970d9f461a654bc3248a9843c3a3b27808f
GUILHERME FACCINI RODRIGUES <faccini.g@gmail.com>
2023-09-19 16:22:00 -0300

    Update util.R

--- R/util.R
+++ R/util.R
@@ -10,6 +10,10 @@ map_lgl <- function(x, fun, ...) {
   vapply(x, fun, FUN.VALUE = logical(1), ...)
 }
 
+map_int <- function(x, fun, ...) {
+  vapply(x, fun, FUN.VALUE = integer(1), ...)
+}
+
 discard <- function(x, .p, ...) {
   discarted <- map_lgl(x, .p, ...)
   x[!discarted]
@@ -27,3 +31,30 @@ check_vector <- function(df, name, is_of_type, type) {
 cat_glue <- function(msg, .envir = parent.frame()) {
   cat(glue(msg, .envir = .envir), "\n")
 }
+
+# remove_one <- function(lst, .p) {
+#   for (i in seq_along(lst)) {
+#     if (.p(lst[[i]])) {
+#       lst <- lst[-i]
+#       return(lst)
+#     }
+#   }
+#   stop("No item was found!")
+# }
+
+remove <- function(lst, .p, ...) {
+  Reduce(
+    function(a, b) {
+      if (.p(b, ...)) return(a)
+      append(a, list(b))
+    },
+    x = lst,
+    init = list()
+  )
+}
+
+
+
+
+
+
