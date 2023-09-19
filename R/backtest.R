6191294aeb9ffd80726ec002556fdd0ed4e63a8e
GUILHERME FACCINI RODRIGUES <faccini.g@gmail.com>
2023-09-19 16:20:55 -0300

    redesign backtest.R

--- R/backtest.R
+++ R/backtest.R
@@ -2,41 +2,28 @@ Backtest <- R6::R6Class(
   "Backtest",
   # public ----
   public = list(
-    current_idx = 0L,
-    order_idx = 0L,
-    trade_idx = 0L,
-    data = NULL,
+    current_bar = 0L,
     orders = list(),
     trades = list(),
-    strategy = NULL,
+    data = NULL,
     reporter = NULL,
-    # trade_cost = function(price = 0, order = NULL) {0}
-    cash = NA_real_,
-    initial_cash = NA_real_,
-    next_trade = function() {
-      self$trade_idx <- trade_idx + 1L
-      self$trade_idx
+    begin = function(...) {
+      stop("`begin` method must be created by user!")
     },
-    next_order = function() {
-      self$order_idx <- self$order_idx + 1L
-      self$order_idx
+    on_bar = function(current_bar) {
+      stop("`on_bar` method must be created by user!")
+    },
+    initialize = function(reporter = Console_reporter()) {
+      self$set_reporter(reporter)
     },
     set_data = function(data) {
-      # TODO: testar se o df possui requisitos mínimos
-      # Possui:
-      #   * date
-      #   * OHLC
+      # talvez esta função deva se tornar um active binding
       check_vector(data, name = "date", is_of_type = \(x) inherits(x, "Date"), type = "Date")
       check_vector(data, name = "open", is_of_type = is.numeric, type = "numeric")
       check_vector(data, name = "high", is_of_type = is.numeric, type = "numeric")
       check_vector(data, name = "low", is_of_type = is.numeric, type = "numeric")
       check_vector(data, name = "close", is_of_type = is.numeric, type = "numeric")
       self$data <- data
-      invisible(self)
-    },
-    set_strategy = function(strategy) {
-      # testar se é da classe strategy
-      self$strategy <- strategy
       invisible(self)
     },
     set_reporter = function(reporter) {
@@ -57,75 +44,153 @@ Backtest <- R6::R6Class(
     #     * initial_cash = 100000,
     #     * from_start = TRUE,
     run = function(...) {
+      private$renew()
+      self$begin(...)
       # avalia se todos os requisitos para rodar são atendidos
       # Possui:
-      #   * strategy
       #   * data
       # Se reporter não for definido usar "ReporterSimple"(avaliar somente estatísticas básicas)
       # iniciar reporter
-      # iniciar strategy
       # avança para o primeira data sem NA
-      private$find_begin()
+      private$find_initial_bar()
       # roda o backtest
-      private$run()
+      private$backtest()
+      self
+    },
+    buy = function(size = 100L) {
+      private$add_order(
+        size = size,
+        side = BUY,
+        type = MARKET
+      )
+    },
+    sell = function(size = 100L) {
+      private$add_order(
+        size = -size,
+        side = SELL,
+        type = MARKET
+      )
+    },
+    buy_start = function(size = 100L, start_price) {
+      private$add_order(
+        size = size,
+        side = BUY,
+        type = START,
+        start_price = start_price
+      )
+    },
+    sell_start = function(size = 100L, start_price) {
+      private$add_order(
+        size = -size,
+        side = SELL,
+        type = START,
+        start_price = start_price
+      )
+    },
+    buy_stop = function(size = 100L, stop_price) {
+      private$add_order(
+        size = size,
+        side = BUY,
+        type = STOP,
+        stop_price = stop_price
+      )
+    },
+    sell_stop = function(size = 100L, stop_price) {
+      private$add_order(
+        size = -size,
+        side = SELL,
+        type = STOP,
+        stop_price = stop_price
+      )
+    },
+    last_line = function() {self$data[self$current_idx, ]},
+    open = function() {self$data$open[self$current_idx]},
+    high = function() {self$data$high[self$current_idx]},
+    low = function() {self$data$low[self$current_idx]},
+    close = function() {self$data$close[self$current_idx]}
+
+  ),
+  # private----
+  private = list(
+    order_idx = 0L,
+    trade_idx = 0L,
+    renew = function() {
+      private$order_idx <- 0L
+      private$trade_idx <- 0L
+      self$current_bar <- 0L
+      self$orders <- list()
+      self$trades <- list()
+      self
     },
     add_order = function(symbol, size, side, type, ...) {
-      order <- new_order(idx = private$next_order(),
-                         date_idx = self$current_idx,
+      order <- new_order(order_idx = private$next_order(),
+                         bar = self$current_bar,
                          symbol = symbol,
                          size = size,
                          side = side,
                          type = type,
                          ...)
-      self$orders <- append(self$orders, list(order))
+      private$orders <- append(self$orders, list(order))
       invisible(self)
     },
-    last_line = function() {self$data[self$current_idx, ]}
-  ),
-  # private----
-  private = list(
+    next_trade = function() {
+      private$trade_idx <- private$trade_idx + 1L
+      private$trade_idx
+    },
+    next_order = function() {
+      private$order_idx <- private$order_idx + 1L
+      private$order_idx
+    },
     next_bar = function() {
-      self$current_idx <- self$current_idx + 1L
+      self$current_bar <- self$current_bar + 1L
+      self$current_bar
+    },
+    find_initial_bar = function() {
+      max_NAs <- map_int(self$data,
+                         function(.x) {
+                           w <- which(is.na(.x))
+                           if (length(w) == 0L) return(0L)
+                           max(w)
+                         })
+      self$current_bar <- max(max_NAs) + 1L
+      self$current_bar
+    },
+    backtest = function() {
+      last_bar <- nrow(self$data)
+      while (private$next_bar() <= last_bar) {
+        private$fill_orders()
+        self$on_bar()
+      }
       self
     },
     fill_orders = function() {
-      orders <- self$strategy$orders
+      orders <- private$orders
       if (length(orders) == 0L) return(self)
 
       new_trades <- Reduce(function(lst, order) {
-        trade <- fill_order(order, bt = self)
+        trade <- self$broker$fill_order(order)
         if (is.null(trade)) return(lst)
-        self$portifolio$add_trade(trade)
+
         append(lst, list(trade))
       },
       x = orders,
       init = list())
 
-      self$trades <- append(self$trades, new_trades)
-      self
-    },
-    find_begin = function() {
-      self$current_idx <- 1L
+      if (length(new_trades) > 0L) {
+        removed_orders <- map_int(new_trades, \(t) t$order_idx)
+        self$orders <- remove(orders, \(o) o$order_idx %in% removed_orders)
+        self$trades <- append(self$trades, new_trades)
+      }
 
-      while (TRUE) {
-        self$reporter$on_prenext(self$current_idx)
-        any_na <- any(map_lgl(self$data, function(x) is.na(x[self$current_idx])))
-        if (!any_na) break
-        private$next_bar()
-      }
-    },
-    run = function() {
-      nro_bars <- nrow(self$data)
-      while (self$current_idx <= nro_bars) {
-        private$fill_orders()
-        self$strategy$next_bar(self$current_idx)
-        self$reporter$on_next(self$current_idx)
-        private$next_bar()
-      }
+      self
     }
   )
 )
 
+
+# Fill order -------------------------------------------------------------------
+# Não vai funcionar como desejado - o R6 só usa as funções definidas no pacote
+# não podendo ser substituidas pelo usuário
 
 fill_order <- function(ord, bt) {
   UseMethod("make_trade")
